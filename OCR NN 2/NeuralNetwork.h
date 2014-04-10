eaf23c4741e41d69cc6ec68ffa93f7f7622872d8
Harmen Klink <harmen.klink@gmail.com>
2014-04-10 13:47:23 +0200

    Main in vcxproj, import constructor and export

--- OCR NN 2/NeuralNetwork.h
+++ OCR NN 2/NeuralNetwork.h
@@ -8,10 +8,12 @@ class NeuralNetwork
 {
 public:
 	NeuralNetwork(const std::vector<unsigned int> topology);
+	NeuralNetwork(const std::string &importFile);
 	~NeuralNetwork();
 	void feedForward(const std::vector<float> &input);
 	void backPropagate(const std::vector<float> &target);
 	std::vector<float>& getOutput();
+	void export(const std::string filename);
 
 	unsigned int num_layers() const { return static_cast<unsigned int>(layers.size()); }
 
