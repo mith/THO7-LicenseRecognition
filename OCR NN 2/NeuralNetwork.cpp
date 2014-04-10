f5d307f431f629c01cd03df5dd22607c30c44ef0
Harmen Klink <harmen.klink@gmail.com>
2014-04-10 20:33:36 +0200

    Backpropagation Bias Neuron bug fixes

--- OCR NN 2/NeuralNetwork.cpp
+++ OCR NN 2/NeuralNetwork.cpp
@@ -107,7 +107,7 @@ void NeuralNetwork::feedForward(const vector<float> &input) {
 // Author: Harmen Klink
 void NeuralNetwork::backPropagate(const std::vector<float> &target) {
 	NeuronLayer &output_layer = layers.back();
-	for (unsigned int neuron = 0; neuron < output_layer.num_nodes() - 1; ++neuron) {
+	for (unsigned int neuron = 0; neuron < output_layer.num_mut_nodes(); ++neuron) { // NO BIAS NEURON IN OUTPUT LAYER ANYMORE????
 		float neuron_error = target[neuron] - output_layer.output_values[neuron]; // Calculate neuron error
 		//Calculate gradient for this output neuron by multiplying the neuron error with the derivative of the outputvalue of tanh
 		output_layer.gradients[neuron] = neuron_error * (1 - (tanh(output_layer.output_values[neuron]) * tanh(output_layer.output_values[neuron])));
@@ -119,7 +119,7 @@ void NeuralNetwork::backPropagate(const std::vector<float> &target) {
 		for (unsigned int current_neuron = 0; current_neuron < current_layer.num_nodes(); ++current_neuron) {
 			
 			float sum = 0.0; // Sum of this neuron's contribution to the errors of the next layer
-			for (unsigned int next_neuron = 0; next_neuron < layers[layer + 1].output_values.size() - 1; ++next_neuron) {
+			for (unsigned int next_neuron = 0; next_neuron < layers[layer + 1].num_mut_nodes(); ++next_neuron) {
 				sum += layers[layer].weights[current_neuron][next_neuron] * layers[layer + 1].gradients[next_neuron];
 			}
 			layers[layer].gradients[current_neuron] = sum * (1 - (tanh(current_layer.output_values[current_neuron]) * tanh(current_layer.output_values[current_neuron])));
